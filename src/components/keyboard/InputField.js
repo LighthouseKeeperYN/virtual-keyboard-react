9ee377ce8c3356b040e2f6c14a3bbd98a941b066
LighthouseKeeperYN <freesaber@yandex.ru>
2020-01-13 18:05:43 +0300

    feat: imput field

---
+++ src/components/keyboard/InputField.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InputField() {
+  return (
+    <input className="input-field"  type="text" maxLength={20}>
+
+    </input>
+  )
+}
+
+export default InputField
--- src/components/keyboard/Keyboard.js
+++ src/components/keyboard/Keyboard.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Key from './Key';
 import eng from './alphabets/eng';
+import InputField from './InputField';
 // import rus from './alphabets/rus';
 
 const Keyboard = () => {
   return (
-    <div className="keyboard-wrapper">
-      {Object.entries(eng).map(([keycode, action]) => (
-        <Key text={action.default} isSpecial={!!action.isSpecial} keycode={keycode} key={keycode} />
-      ))}
-    </div>
+    <Fragment>
+      <InputField />
+      <div className="keyboard-wrapper">
+        {Object.entries(eng).map(([keycode, action]) => (
+          <Key
+            text={action.default}
+            isSpecial={!!action.isSpecial}
+            keycode={keycode}
+            key={keycode}
+          />
+        ))}
+      </div>
+    </Fragment>
   );
 };
 
