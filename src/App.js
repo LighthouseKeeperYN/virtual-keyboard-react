6b96c49c63a30e1fb89676e779e1cdc8dc59ff23
LighthouseKeeperYN <freesaber@yandex.ru>
2020-01-13 17:53:22 +0300

    feat: todo list gui

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './styles/base.scss';
 import './styles/styles.scss';
 
 import Keyboard from './components/keyboard/Keyboard';
+import ToDoList from './components/toDoList/ToDoList';
 
 const App = () => {
   return (
     <main className="main-wrapper">
+      <ToDoList />
       <Keyboard />
     </main>
   );
