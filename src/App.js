47902d0916390a370048f96440064cb66ba4a1c3
LighthouseKeeperYN <freesaber@yandex.ru>
2023-09-08 08:33:20 +0200

    chore: update packages, feat: add hint and improve styles

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import ToolPanelState from './context/GlobalState';
 import Keyboard from './components/keyboard/Keyboard';
 import ToDoList from './components/toDoList/ToDoList';
 import Alert from './components/Alert';
+import Hint from './components/Hint';
 
 const App = () => (
   <ToolPanelState>
     <main className="main-wrapper">
+      <Hint />
       <ToDoList />
       <Alert />
       <Keyboard />
---
+++ src/components/Hint.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Alert = () => {
+  return (
+    <div className="hint">
+      <h2>This is a virtual keyboard with TODO list</h2>
+      <p>You can write the text using your physical keyboard or by clicking on virtual keys</p>
+      <p>To add the item press or click "Enter"</p>
+    </div>
+  );
+};
+
+export default Alert;
